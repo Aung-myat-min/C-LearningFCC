7abe72ce9bc1fc4a05e859b8627a94a549557208
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-15 15:16:05 +0100

    init: project & feat: fcc2 completed

---
+++ fCC2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(4, 4000);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+// Code Challenge
+int firstValue = 500;
+int secondValue = 600;
+int largeValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largeValue);
