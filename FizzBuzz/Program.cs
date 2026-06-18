983751e3a46124ff0c8a0f024023dccedaa7e2d1
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-18 13:51:13 +0100

    feat: fizz buzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else
+        Console.WriteLine(i);
+
+}
