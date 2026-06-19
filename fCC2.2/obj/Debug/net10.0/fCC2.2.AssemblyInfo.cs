20b584ff1b6aaeddc507024f3a8b5b503665aee2
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-19 13:27:46 +0100

    feat: guided petshop

--- fCC2.2/obj/Debug/net10.0/fCC2.2.AssemblyInfo.cs
+++ fCC2.2/obj/Debug/net10.0/fCC2.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73047aef3e344f1ae0444dbc3895d6b6c71e89eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7b375bb5e95ce7805bb2804ed4b1025f7f3ffe")]
 [assembly: System.Reflection.AssemblyProductAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
