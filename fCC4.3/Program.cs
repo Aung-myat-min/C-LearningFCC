35667dba42f50f77b87e520005118fbce2e8c4b2
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-22 00:44:50 +0100

    feat: fcc4.3 challenge 1 completed

---
+++ fCC4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] wordArray = pangram.Split(" ");
+string[] reversedArray = new string[wordArray.Length];
+
+for (int i = 0; i < wordArray.Length; i++)
+{
+    string tempStr = wordArray[i];
+    char[] tempCharArr = tempStr.ToCharArray();
+    Array.Reverse(tempCharArr);
+
+    reversedArray[i] = new string(tempCharArr);
+}
+
+Console.WriteLine($"{string.Join(" ", reversedArray)}");
