76784e58321f0b988320e84589a9537939ed9e57
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-22 00:06:38 +0100

    feat: fcc4.2 challenge 1 completed

---
+++ fCC4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string message = "";
+float total = 0.0f;
+
+foreach (string str in values)
+{
+    float num = 0.0f;
+    if (float.TryParse(str, out num))
+    {
+        total += num;
+    }
+    else
+    {
+        message += str;
+    }
+}
+
+Console.WriteLine($"Message: {message}\nTotal: {total}");
