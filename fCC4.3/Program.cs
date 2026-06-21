78772812505aa4b521297a2c353991858c7cb6eb
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-22 00:49:20 +0100

    feat: fcc4.3 challenge 2 completed

--- fCC4.3/Program.cs
+++ fCC4.3/Program.cs
@@ -1,14 +1,24 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
-string[] wordArray = pangram.Split(" ");
-string[] reversedArray = new string[wordArray.Length];
+﻿// string pangram = "The quick brown fox jumps over the lazy dog";
+// string[] wordArray = pangram.Split(" ");
+// string[] reversedArray = new string[wordArray.Length];
 
-for (int i = 0; i < wordArray.Length; i++)
+// for (int i = 0; i < wordArray.Length; i++)
+// {
+//     string tempStr = wordArray[i];
+//     char[] tempCharArr = tempStr.ToCharArray();
+//     Array.Reverse(tempCharArr);
+
+//     reversedArray[i] = new string(tempCharArr);
+// }
+
+// Console.WriteLine($"{string.Join(" ", reversedArray)}");
+
+// Challenge 2
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderIDs = orderStream.Split(",");
+Array.Sort(orderIDs);
+
+foreach (string orderID in orderIDs)
 {
-    string tempStr = wordArray[i];
-    char[] tempCharArr = tempStr.ToCharArray();
-    Array.Reverse(tempCharArr);
-
-    reversedArray[i] = new string(tempCharArr);
+    Console.WriteLine($"{orderID}\t" + ((orderID.Length != 4) ? "- Error" : ""));
 }
-
-Console.WriteLine($"{string.Join(" ", reversedArray)}");
