5cc432af94d151bccae5364eb8b7090fc76ed925
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-23 01:49:42 +0100

    feat: fcc4.5 challenge completed

--- fCC2.2/obj/Debug/net10.0/fCC2.2.AssemblyInfo.cs
+++ fCC2.2/obj/Debug/net10.0/fCC2.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ffcbb36ca0d6bb76a90e5fef6495477a1e6880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78772812505aa4b521297a2c353991858c7cb6eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fCC2.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ fCC4.5/Program.cs
@@ -0,0 +1,26 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string divStart = "<div>";
+const string divEnd = "</div>";
+const string spanStart = "<span>";
+const string spanEnd = "</span>";
+
+int divStartIndex = input.IndexOf(divStart);
+divStartIndex += divStart.Length;
+int divEndIndex = input.IndexOf(divEnd);
+
+output = input.Substring(divStartIndex, divEndIndex - divStartIndex);
+output = output.Replace("trade", "reg");
+
+int spanStartIndex = output.IndexOf(spanStart);
+spanStartIndex += spanStart.Length;
+int spanEndIndex = output.IndexOf(spanEnd);
+
+quantity = output.Substring(spanStartIndex, spanEndIndex - spanStartIndex);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
