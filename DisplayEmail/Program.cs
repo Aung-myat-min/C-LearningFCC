0c022459a6467ad46079eef02ee35bb4c00909c6
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-06-24 00:40:35 +0100

    feat: completed DisplayEmail exercise

---
+++ DisplayEmail/Program.cs
@@ -0,0 +1,37 @@
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    string firstName = corporate[i, 0];
+    string lastName = corporate[i, 1];
+
+    DisplayEmail(firstName, lastName, "contoso.com");
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    string firstName = corporate[i, 0];
+    string lastName = corporate[i, 1];
+
+    DisplayEmail(firstName, lastName);
+}
+
+void DisplayEmail(string firstName, string lastName, string domain = "contoso.com")
+{
+    firstName = firstName.Substring(0, 2).ToLower();
+    lastName = lastName.ToLower();
+    Console.WriteLine($"{firstName}{lastName}@{domain}");
+}
